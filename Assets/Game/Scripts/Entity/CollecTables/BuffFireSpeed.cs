dcfff88b5fc15ee0a7850e5f443023a9af415c0a
Onurcan Koçak <onurcn_kck75@hotmail.com>
2022-03-12 19:19:22 +0300

    Enemy Take Damage And Player Take Damage Added

--- Assets/Game/Scripts/Entity/CollecTables/BuffFireSpeed.cs
+++ Assets/Game/Scripts/Entity/CollecTables/BuffFireSpeed.cs
@@ -6,19 +6,43 @@ public class BuffFireSpeed : MonoBehaviour, ICollecTable
 {
     [SerializeField]
     private float speed;
+
+    [SerializeField]
+    private float value;
+
+    private float maxDistance;
+
+    private Vector3 spawnPosition;
+
+    private void Awake() 
+    {
+        maxDistance = Camera.main.orthographicSize *2 + 5;    
+    }
+
+
     private void Update() 
     {
-        transform.position += -transform.forward * Time.deltaTime * speed;    
+        transform.position -= transform.forward * Time.deltaTime * speed;  
+
+        if (Vector3.Distance(transform.position, spawnPosition) > maxDistance)
+        {
+            gameObject.SetActive(false);
+        }  
     }
     public void Collect()
     {
-        Debug.Log("asd");
+       gameObject.SetActive(false);
+       Player.Instance.FireTime *= value;
     }
 
     public void Spawn(Vector3 position)
     {
-        
         transform.position = position;
         gameObject.SetActive(true);
+
+        LeanTween.moveX(gameObject, 10, .4f).setOnComplete
+        (()=>
+                LeanTween.moveX(gameObject, -20, .8f).setLoopPingPong().setEaseInOutCubic()
+        );
     }
 }
